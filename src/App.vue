bd48c3d240116ef25eae93c59e6e4845ac5373a9
Maele <maelejn@gmail.com>
2022-08-22 18:35:37 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator,
+}
+}
+</script>
+
+<style>
+  body{
+    background: #31475e;
+  }
+</style>
